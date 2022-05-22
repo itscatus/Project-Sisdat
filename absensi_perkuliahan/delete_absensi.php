34839e0045fff4f4f007123a9417ec560da73bc8
fel <100121123+itscatus@users.noreply.github.com>
2022-05-23 00:17:35 +0700

    Delete absensi_perkuliahan directory

--- absensi_perkuliahan/delete_absensi.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'index.php';
-if(isset($_GET['del_id'])){
-    $delete = mysqli_query($koneksi, "DELETE from absensi where id_absensi = '".$_GET['del_id']."'");
-    echo '<script>window.location="data_absensi.php"</script>';
-}
-?>
--- absensi_perkuliahan/delete_matkul.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'index.php';
-if(isset($_GET['del_id'])){
-    $delete = mysqli_query($koneksi, "DELETE from perkuliahan where kode_matkul = '".$_GET['del_id']."'");
-    echo '<script>window.location="data_matkul_adm.php"</script>';
-}
-?>
