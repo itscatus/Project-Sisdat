bb7db261840d93e669d4cd65112041e71911052a
fel <100121123+itscatus@users.noreply.github.com>
2022-05-23 00:25:12 +0700

    Delete absensikuliah directory

--- absensikuliah/delete_ruangan.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'index.php';
-if(isset($_GET['del_id'])){
-    $delete = mysqli_query($koneksi, "DELETE from ruangan where kode_ruangan = '".$_GET['del_id']."'");
-    echo '<script>window.location="data_ruangan_adm.php"</script>';
-}
-?>
